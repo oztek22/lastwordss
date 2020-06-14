9a18e38ae89967dfe9857c0ded4050c2e5e0870f
Yash Dave <yash.dave@usercentrics.com>
2020-06-14 16:11:12 +0530

    send page design updated

--- src/views/Send/Send.js
+++ src/views/Send/Send.js
@@ -3,14 +3,7 @@ import Check from '../../components/check/check'
 import './Send.css';
 
 function Send() {
-  const lastWords = 'I am not sure if we will make it to 2020 so tell me your last words...';
-  const tragedies = [
-    {name: 'A deadly virus', isChecked: true, id: 1},
-    {name: 'Big ass earthquakes', isChecked: true, id: 2},
-    {name: 'Forest Fire', isChecked: true, id: 3},
-    {name: 'Alien Invasion', isChecked: false, id: 4},
-    {name: 'Dinosaurs Attack!', isChecked: false, id: 5}
-  ];
+  const lastWords = "I'm not sure we're gonna make it alive out of(or through) 2020. What would be your last words for me?";
 
   const [message, setMessage] = useState('');
 
@@ -19,18 +12,14 @@ function Send() {
 
   return (
     <div className="Send">
-      <div className="send-header">
-        {tragedies.map(tragedy => {
-          return <div key={tragedy.id} className="send-header-list">
-            <span className="send-header-checkbox">{tragedy.isChecked ? <Check /> : null }</span>
-            <span className="send-header-text">{ tragedy.name }</span>
-          </div>
-        })}
+      <div className="title">
+        <div className="title-1">last</div>
+        <div className="title-2">wordss</div>
       </div>
       <div className="send-main">
         <div className="send-main-title">{lastWords}</div>
-        <textarea className="send-main-message"></textarea>
-        <div className="send-main-submit">Write anonymously...</div>
+        <input type="text" className="send-main-message" placeholder="don’t hold back, write it down"></input>
+        <div className="send-main-submit">POST ANONYMOUSLY</div>
       </div>
     </div>
   );
